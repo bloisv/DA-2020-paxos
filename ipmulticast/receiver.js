15e91b0f69a6e6e4043a3314f80447015a711f60
Long Le <hoanglong25588@gmail.com>
2020-09-29 22:26:51 +0200

    allowing multiple process listening on same port

--- ipmulticast/receiver.js
+++ ipmulticast/receiver.js
@@ -3,20 +3,16 @@ const MULTICAST_ADDR = process.env.APP_MULTICAST_ADDR;
 
 //Multicast Client receiving sent messages
 var dgram = require("dgram");
-var receiver = dgram.createSocket("udp4");
+var receiver = dgram.createSocket({ type: "udp4", reuseAddr: true });
 
 receiver.on("listening", function () {
   var address = receiver.address();
-  console.log(
-    "UDP Client listening on " + address.address + ":" + address.port
-  );
+  console.log("UDP Client listening on ", address.address, ":", address.port);
   receiver.addMembership(MULTICAST_ADDR);
 });
 
 receiver.on("message", function (message, remote) {
-  console.log(
-    "Receiving from: " + remote.address + ":" + remote.port + " - " + message
-  );
+  console.log("Msg from: ", remote.address, ":", remote.port, " - ", message);
 });
 
 receiver.bind(MULTICAST_PORT);
